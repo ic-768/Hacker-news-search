02662a59e307ae8d3298224d69ab141bd544579c
ixrisostomakis <ixrisostomakis@joltie.com>
2025-02-28 20:50:30 +0200

    Some nice animation

--- src/components/auto-suggest/auto-suggest.tsx
+++ src/components/auto-suggest/auto-suggest.tsx
@@ -1,13 +1,13 @@
 import { Input } from "@/components/ui/input";
 import { useSuggestions } from "@/hooks/useSuggestions";
 import ListItem from "./list-item";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function AutoSuggest() {
   const { setQuery, isOpen, isFetching, query, stories } = useSuggestions();
 
   return (
-    <div role="combobox" aria-expanded={isOpen}>
-      {isFetching && <span>Loading...</span>}
+    <div className="w-96" role="combobox" aria-expanded={isOpen}>
       <Input
         type="text"
         placeholder="Search Hacker News..."
@@ -17,16 +17,21 @@ export default function AutoSuggest() {
         aria-controls="suggestion-list"
       />
       {isOpen && stories.length > 0 && (
-        <ul
+        <motion.ul
           id="suggestion-list"
           className=" border rounded shadow-md max-h-96 overflow-y-auto"
           role="listbox"
+          layout
         >
-          {stories.map((story) => (
-            <ListItem story={story} key={story.objectID} />
-          ))}
-        </ul>
+          <AnimatePresence initial={false}>
+            {stories.map((story) => (
+              <ListItem query={query} story={story} key={story.objectID} />
+            ))}
+          </AnimatePresence>
+        </motion.ul>
       )}
+
+      {isFetching && <span>Loading...</span>}
     </div>
   );
 }
--- src/components/auto-suggest/list-item.tsx
+++ src/components/auto-suggest/list-item.tsx
@@ -1,13 +1,46 @@
 import { Story } from "@/types";
+import { motion } from "framer-motion";
 
-export default function ListItem({ story }: { story: Story }) {
+interface ListItemProps {
+  query: string;
+  story: Story;
+}
+export default function ListItem({ story, query }: ListItemProps) {
+  // split title into highlighted and non-highlighted spans
+  const getHighlightedText = (text: string) => {
+    if (!query) return text;
+
+    // Add a literal backslash before each special character ($& is the  matched char)
+    const escapedFilter = query.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    const regex = new RegExp(`(${escapedFilter})`, "gi");
+    return text.split(regex).map((part, index) => {
+      return part.toLowerCase() === query.toLowerCase() ? (
+        <span className="relative" key={index}>
+          <motion.span
+            layoutId={`underline-${story.objectID}`}
+            className="absolute bottom-0 left-0 h-0.5 w-full bg-orange-300"
+          />
+          {part}
+        </span>
+      ) : (
+        <span key={index}>{part}</span>
+      );
+    });
+  };
+
   return (
-    <li className="p-2 hover:bg-gray-100 cursor-pointer">
-      <p className="font-semibold">{story.title || story.story_title}</p>
+    <motion.li
+      layoutId={story.objectID}
+      exit={{ scale: 0 }}
+      className="p-2 hover:bg-gray-100 cursor-pointer"
+    >
+      <p className="font-semibold">
+        {getHighlightedText(story.title || story.story_title)}
+      </p>
       <p className="text-sm text-gray-600">
         By {story.author} | {story.points} points | {story.num_comments}{" "}
         comments
       </p>
-    </li>
+    </motion.li>
   );
 }
